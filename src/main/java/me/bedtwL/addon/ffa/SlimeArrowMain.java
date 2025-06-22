ff23a8b403ab01c6b991e11106f0112e0c972f06
itsglobally <itsglobally1324@gmail.com>
2025-06-22 01:01:35 +0000

    ewe

---
+++ src/main/java/me/bedtwL/addon/ffa/SlimeArrowMain.java
@@ -0,0 +1,25 @@
+package me.bedtwL.addon.ffa;
+
+import me.bedtwL.ffa.api.EffectAddon;
+
+public class SlimeArrowMain implements EffectAddon {
+    @Override
+    public String getName() {
+        return "SlimeArrow";
+    }
+
+    @Override
+    public String getAuthor() {
+        return "ItsGlobally";
+    }
+
+    @Override
+    public void onEnable() {
+        new ArrowSlimeShoot().registerArrowShootEffect();
+    }
+
+    @Override
+    public void onDisable() {
+        new ArrowSlimeShoot().unregisterArrowShootEffect();
+    }
+}
