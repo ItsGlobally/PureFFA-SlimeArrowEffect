11c285e861aa1e40ca75cefd32a71685898a1df2
itsglobally <itsglobally1324@gmail.com>
2025-06-25 09:39:36 +0000

    ewe

--- src/main/java/me/itsglobally/addon/ffa/SlimeArrowMain.java
+++ src/main/java/me/itsglobally/addon/ffa/SlimeArrowMain.java
@@ -2,7 +2,9 @@ package me.itsglobally.addon.ffa;
 
 import me.bedtwL.ffa.api.EffectAddon;
 
-public abstract class SlimeArrowMain implements EffectAddon {
+import java.util.UUID;
+
+public class SlimeArrowMain implements EffectAddon {
     @Override
     public Integer getAPIVer() { return 1; };
     @Override
@@ -14,7 +16,10 @@ public abstract class SlimeArrowMain implements EffectAddon {
     public String getAuthor() {
         return "ItsGlobally";
     }
-
+    @Override
+    public UUID authorUUID() {
+        return UUID.fromString("748c3863-c04d-493b-bce3-05a1765a5b56");
+    }
     @Override
     public void onEnable() {
         new ArrowSlimeShoot().registerArrowShootEffect(this);
